9c578f8d6b5c9c1a0a3679c21bf16309a6a5506a
Adecanal <canaladepro@gmail.com>
2025-05-22 16:26:07 +0100

    Add number conversion script

---
+++ 5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const num = process.argv[2];
+
+if (isNaN(num) || num === undefined) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', Math.floor(Number(num)));
+}
